8766bc74b497d33400ba38515e26182dda7bbe88
ZCKevin <ckevin31337@gmail.com>
2023-09-23 13:19:13 -0700

    init player control

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -8,26 +8,54 @@ public class Attack : MonoBehaviour
     // Start is called before the first frame update
     private float LeftEdge, RightEdge;
     [SerializeField] private PlayerMovement Player;
-    
-
+    [SerializeField] private float PreTime = .5f;
+    [SerializeField] private float PostTime = .5f; 
+    enum Status { 
+        IdleStage,
+        PreStage,
+        PostStage,
+        CooldownStage
+    };
+    private Status CurrentStatus; 
+    void SetStatus(Status status)
+    {
+        CurrentStatus = status;
+    }
+    bool IsActive()
+    {
+        return CurrentStatus != Status.IdleStage;
+    }
     void Start()
     {
+        SetStatus(Status.IdleStage);
         RightEdge = (float)Random.Range(2, 4);
         LeftEdge = (float)Random.Range(-4, -2);
         Debug.Log(LeftEdge);
         Debug.Log(RightEdge);
     }
 
+    IEnumerator Trigger()
+    {
+        SetStatus(Status.PreStage);
+        Player.Freeze();
+        yield return new WaitForSeconds(PreTime);
+        AttackEvent();
+        yield return null;
+        Player.UnFreeze();
+        SetStatus(Status.PostStage);
+        yield return new WaitForSeconds(PostTime); 
+        SetStatus(Status.IdleStage);
+        yield return null; 
+    }
     // Update is called once per frame
     void Update()
     {
-        Mouse mouse = Mouse.current;
-        if (mouse.leftButton.wasPressedThisFrame)
-        {
-            AttackEvent();
+        if (Input.GetButtonDown("Fire1")) {
+            Debug.Log("Fire 1 button is triggered");
+            if (!IsActive())
+                StartCoroutine(Trigger());
         }
     }
-
     public void AttackEvent()
     {
         float SightX = Player.GetSightX();
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,11 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    enum Mode
-    {
-        AttackPre,
-        AttackActive,
-        AttackBackSwing,
-        AttackCoolDown,
-        DefendPre,
-        DefendActive,
-        DefendBackSwing,
-        DefendCoolDown,
-    };
     private Rigidbody2D rd;
-    private float speed = 4;
-    private float SightXOffset = 3, SightYOffset = 5;
+    [SerializeField] private float speed = 90;
+    // private float SightXOffset = 3, SightYOffset = 5;
     private float MoveLimit = 9;
+    private int Frozen = 0;
     [SerializeField] private GameObject Sight;
     Transform SightTrans;
     // Start is called before the first frame update
@@ -27,26 +17,39 @@ public class PlayerMovement : MonoBehaviour
     { 
         rd = GetComponent<Rigidbody2D>();
         SightTrans = Sight.transform;
+        Frozen = 0;
     }
-    private float GetX(float x)
+    float ClampSpeed(float dx)
     {
-        if (x > MoveLimit) x = MoveLimit;
-        if (x < -MoveLimit + SightXOffset) x = -MoveLimit + SightXOffset;
-        return x;
+        if (rd.position.x <= -MoveLimit)
+            return Mathf.Max(dx, 0f);
+        if (rd.position.x >= MoveLimit)
+            return Mathf.Min(dx, 0f);
+        if (Frozen > 0) dx = 0f;
+        return dx;
     }
-    // Update is called once per frame
-    
+    // Update is called once per frame 
     void Update()
     {
-        float ns = Input.GetAxisRaw("Horizontal");
-        rd.velocity = new Vector2(ns, 0) * speed;
-        rd.position = new Vector2(GetX(rd.position.x), rd.position.y);
-        SightTrans.position = new Vector2(rd.position.x - SightXOffset, rd.position.y + SightYOffset);
+        float dx = Input.GetAxisRaw("Horizontal");
+        rd.velocity = new Vector2(ClampSpeed(dx), 0) * speed;
+        rd.position = new Vector2(Mathf.Clamp(rd.position.x, -MoveLimit, MoveLimit), rd.position.y);
+        Debug.Log(new Vector2(ClampSpeed(dx), 0) * speed);
+        Debug.Log(rd.position.x);
+        // SightTrans.position = new Vector2(rd.position.x - SightXOffset, rd.position.y + SightYOffset);
         //Debug.Log(SightTrans.position.x);
     }
 
     public float GetSightX()
     {
         return SightTrans.position.x;
+    }
+    public void Freeze()
+    {
+        ++Frozen;
+    }
+    public void UnFreeze()
+    {
+        --Frozen;
     }
 }
