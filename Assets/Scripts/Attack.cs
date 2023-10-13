c1672256fd819dc2fe93f1f3f8983fb08cd93af8
terry489490 <terry489490@gmail.com>
2023-10-14 01:54:10 +0800

    Anim Done

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -9,13 +9,14 @@ public class Attack : MonoBehaviour
     // Start is called before the first frame update
     private PlayerControl Player;
     [SerializeField] private float PreTime = .3f;
+    [SerializeField] private float InTime = .1f;
     [SerializeField] private float PostTime = .5f;
     [SerializeField] private float BlockedPenalty = .5f;
     public float AttackRange = 2f;
     //[SerializeField] private float AttackRange = 1f;
     // [SerializeField] private BoxCollider2D AimPoint;
     private Coroutine lastRoutine = null;
-    private Animator anim;
+    private Animator Anim;
 
     public enum Status { 
         IdleStage,
@@ -49,35 +50,52 @@ public class Attack : MonoBehaviour
     {
         Player = GetComponent<PlayerControl>();
         SetStatus(Status.IdleStage); 
-        anim = GetComponentInChildren<Animator>();
+        Anim = GetComponentInChildren<Animator>();
     }
 
+    private void ResetAnim()
+    {
+        Anim.SetBool("PreAttack", false);
+        Anim.SetBool("InAttack", false);
+        Anim.SetBool("PostAttack", false);
+        Anim.SetBool("InDefense", false);
+        Anim.SetBool("InDizzy", false);
+    }
     IEnumerator Trigger()
     {
         Player.NoteAttack();
         Debug.Log("Start attacking procedure");
-        anim.SetBool("Attack", true);
+        Anim.SetBool("PreAttack", true);
         SetStatus(Status.PreStage);
         yield return new WaitForSeconds(PreTime);
+        Anim.SetBool("PreAttack", false);
+
+        Anim.SetBool("InAttack", true);
         bool IsBlocked = AttackEvent();
+        yield return new WaitForSeconds(InTime);
+        Anim.SetBool("InAttack", false);
+
         Debug.Log("Now sete the status to post stage");
         SetStatus(Status.PostStage);
         if (!IsBlocked)
         {
+            Anim.SetBool("PostAttack", true);
             yield return new WaitForSeconds(PostTime);
+            Anim.SetBool("PostAttack", false);
         }
         else
         {
             Debug.Log("I am blocked, now wait longer");
+            Anim.SetBool("InDizzy", true);
             yield return new WaitForSeconds(PostTime + BlockedPenalty);
+            Anim.SetBool("InDizzy", false);
         }
         Debug.Log("Set to Idel staget");
         SetStatus(Status.IdleStage);
         Player.ResetCancelCnt();
         Debug.Log("Attack end");
-        anim.SetBool("Attack", false);
-
         lastRoutine = null;
+        //ResetAnim();
         yield return null; 
     }
     public void Cancel()
@@ -85,8 +103,8 @@ public class Attack : MonoBehaviour
         if (IsActive())
         { 
             StopCoroutine(lastRoutine);
+            ResetAnim();
             lastRoutine = null;
-            anim.SetBool("Attack", false); 
             SetStatus(Status.IdleStage); 
         }
     }
